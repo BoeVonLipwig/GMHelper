fd4d6ab4e1d0c4786d56d9b47dee5ba8d1ace16e
Shaun Sinclair <sdmsinclair@gmail.com>
2018-09-01 18:38:56 +1200

    :'(

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,33 +6,37 @@ import {StatusBar} from '@ionic-native/status-bar';
 
 import {MyApp} from './app.component';
 import {HomePage} from '../pages/home/home';
-import {MapsPage} from "../pages/maps/maps";
-import {CharacterPage} from "../pages/character/character";
-import {EditCharPage} from "../pages/edit-char/edit-char";
+// import {MapsPage} from "../pages/maps/maps";
+// import {CharacterPage} from "../pages/character/character";
+// import {EditCharPage} from "../pages/edit-char/edit-char";
 import {FirebaseProvider} from '../providers/firebase/firebase';
 
 import {Camera} from '@ionic-native/camera'
 import {AndroidPermissions} from "@ionic-native/android-permissions";
+import {CharacterPageModule} from "../pages/character/character.module";
+import {EditCharPageModule} from "../pages/edit-char/edit-char.module";
+import {MapsPageModule} from "../pages/maps/maps.module";
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    MapsPage,
-    CharacterPage,
-    EditCharPage
+    // MapsPage,
+    // EditCharPage
   ],
   imports: [
     BrowserModule,
+    CharacterPageModule,
+    EditCharPageModule,
+    MapsPageModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     HomePage,
-    MapsPage,
-    CharacterPage,
-    EditCharPage
+    // MapsPage,
+    // EditCharPage
   ],
   providers: [
     StatusBar,
